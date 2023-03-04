f64425e11905eef84604f24ac244485752dd2991
AI Manifest <108227908+AIManifest@users.noreply.github.com>
2023-03-04 12:24:48 +1000

    Update controlnet.py

--- helpers/controlnet.py
+++ helpers/controlnet.py
@@ -1,3 +1,5 @@
+#@title BACKUP CONTROLNET MAIN
+
 import os
 import time
 import itertools
@@ -9,28 +11,29 @@ import random
 import helpers
 from helpers.render import create_first_video
 from pytorch_lightning import seed_everything
-from annotator.util import resize_image, HWC3
 from annotator.hed import HEDdetector
+from annotator.canny import CannyDetector
+from annotator.midas import MidasDetector
+from annotator.util import resize_image, HWC3
 from cldm.model import create_model, load_state_dict
 from cldm.ddim_hacked import DDIMSampler
 from ldm.util import instantiate_from_config
 from IPython import display
 from PIL import Image
 
-
-apply_hed = HEDdetector()
-
+# apply_hed = HEDdetector()
+# apply_canny = CannyDetector()
 def load_controlnet(controlnet_config_path, controlnet_model_path):
     print_flag = False
     verbose = False
     model = create_model(controlnet_config_path).cpu()
-    # model.load_state_dict(load_state_dict('/content/control_sd15_hed.pth', location='cuda'))
+    # model.load_state_dict(load_state_dict(controlnet_model_path, location='cuda'))
     pl_sd = torch.load(controlnet_model_path, map_location="cuda")
     try:
         sd = pl_sd["state_dict"]
     except:
         sd = pl_sd
-    torch.set_default_dtype(torch.float16)
+    # torch.set_default_dtype(torch.float16)
     # model = instantiate_from_config("/content/ControlNet/models/cldm_v15.yaml")
     torch.set_default_dtype(torch.float32)
     m, u = model.load_state_dict(sd, strict=False)
@@ -46,46 +49,37 @@ def load_controlnet(controlnet_config_path, controlnet_model_path):
     save_memory = False
     return model
 
-
-
-def process(control, input_image, prompt, a_prompt, n_prompt, num_samples, image_resolution, detect_resolution, ddim_steps, guess_mode, strength, scale, seed, eta, save_memory=False):
-    # controlnet_config_path = control.controlnet_config_path
-    # controlnet_model_path = control.controlnet_model_path
-    # print_flag = False
-    # verbose = False
-    # model = create_model(controlnet_config_path).cpu()
-    # # model.load_state_dict(load_state_dict('/content/control_sd15_hed.pth', location='cuda'))
-    # pl_sd = torch.load(controlnet_model_path, map_location="cuda")
-    # try:
-    #     sd = pl_sd["state_dict"]
-    # except:
-    #     sd = pl_sd
-    # torch.set_default_dtype(torch.float16)
-    # # model = instantiate_from_config("/content/ControlNet/models/cldm_v15.yaml")
-    # torch.set_default_dtype(torch.float32)
-    # m, u = model.load_state_dict(sd, strict=False)
-    # if print_flag:
-    #     if len(m) > 0 and verbose:
-    #         print("missing keys:")
-    #         print(m)
-    #     if len(u) > 0 and verbose:
-    #         print("unexpected keys:")
-    #         print(u)
-    # model = model.cuda()
-    # ddim_sampler = DDIMSampler(model)
-    # save_memory = False
+def process(control, input_image, prompt, a_prompt, n_prompt, num_samples, image_resolution, detect_resolution, ddim_steps, guess_mode, strength, scale, seed, eta, low_threshold, high_threshold):
     model = control.model
     ddim_sampler = control.ddim_sampler
     save_memory = control.save_memory
     with torch.no_grad():
-        input_image = HWC3(input_image)
-        detected_map = apply_hed(resize_image(input_image, detect_resolution))
-        detected_map = HWC3(detected_map)
-        img = resize_image(input_image, image_resolution)
-        H, W, C = img.shape
+        if control.controlnet_model_type == "apply_hed":
+            print("Applying HED Detection")
+            input_image = HWC3(input_image)
+            detected_map = control.controlnet_apply_type(resize_image(input_image, detect_resolution))
+            detected_map = HWC3(detected_map)
+            img = resize_image(input_image, image_resolution)
+            H, W, C = img.shape
+            detected_map = cv2.resize(detected_map, (W, H), interpolation=cv2.INTER_LINEAR)
+        
+        elif control.controlnet_model_type == "apply_canny":
+            print("Applying Canny Detection")
+            img = resize_image(HWC3(input_image), image_resolution)
+            H, W, C = img.shape
 
-        detected_map = cv2.resize(detected_map, (W, H), interpolation=cv2.INTER_LINEAR)
+            detected_map = control.controlnet_apply_type(img, low_threshold, high_threshold)
+            detected_map = HWC3(detected_map)
+        
+        else:
+            print("Applying Midas Detection")
+            input_image = HWC3(input_image)
+            detected_map, _ = control.controlnet_apply_type(resize_image(input_image, detect_resolution))
+            detected_map = HWC3(detected_map)
+            img = resize_image(input_image, image_resolution)
+            H, W, C = img.shape
 
+        detected_map = detected_map
         controlx = torch.from_numpy(detected_map.copy()).float().cuda() / 255.0
         controlx = torch.stack([controlx for _ in range(num_samples)], dim=0)
         controlx = einops.rearrange(controlx, 'b h w c -> b c h w').clone()
@@ -117,13 +111,15 @@ def process(control, input_image, prompt, a_prompt, n_prompt, num_samples, image
         x_samples = (einops.rearrange(x_samples, 'b c h w -> b h w c') * 127.5 + 127.5).cpu().numpy().clip(0, 255).astype(np.uint8)
 
         results = [x_samples[i] for i in range(num_samples)]
-    return [detected_map] + results
+    return [detected_map] + results if control.controlnet_model_type == "apply_hed" or "apply_depth" else [255 - detected_map] + results
 
 def generate_control(control):
     if control.generate_frames:
         video_path = control.video_path
         frames_path = control.IN_dir
         helpers.animation.vid2frames(video_path, frames_path, n=1, overwrite=True)
+    else:
+        print(f"Frames Path Exists Already at {control.IN_dir}, Skipping frame extraction.")
     input_image = control.IN_dir
     prompt = control.prompt
     a_prompt = control.a_prompt
@@ -137,20 +133,24 @@ def generate_control(control):
     scale = control.scale
     seed = control.seed
     eta = control.eta
+    low_threshold = control.canny_low_threshold
+    high_threshold = control.canny_high_threshold
     start_idx = 0
+    max_frames = len(os.listdir(control.IN_dir))
+    if not os.path.exists(control.OUT_dir):
+        os.makedirs(control.OUT_dir)
     if control.resume_control:            
         start_idx = start_idx - 1
         IN_idx = control.img_idx
         last_idx = start_idx-1
-    img_idx = 0
+    img_idx = 1
     if control.resume_control:
         img_idx = control.img_idx
-    while img_idx < control.max_frames:
+    while img_idx < max_frames:
         input_img = os.path.join(control.IN_dir, f"{img_idx:05}.jpg")
-        img_idx+=1           
         IN_img = input_img
         IN_img = Image.open(IN_img)
-        print(f"Input Image: {input_img}")
+        print(f"\033[35mInput Image\033[0m: {input_img}")
         IN_array = np.asarray(IN_img, dtype=np.uint8)
         input_image = IN_array 
         output = ''
@@ -189,11 +189,11 @@ def generate_control(control):
             f"\033[33mScale\033[0m: {control.scale} - \033[32mStrength\033[0m: {control.strength} - \033[35mSeed\033[0m: {control.seed}"
         )
 
-        image = process(control, input_image, prompt, a_prompt, n_prompt, num_samples, image_resolution, detect_resolution, ddim_steps, guess_mode, strength, scale, seed, eta)
+        image = process(control, input_image, prompt, a_prompt, n_prompt, num_samples, image_resolution, detect_resolution, ddim_steps, guess_mode, strength, scale, seed, eta, low_threshold, high_threshold)
         img = Image.fromarray(image[1])
         img.save(f"{control.OUT_dir}/{control.IN_batch}_{img_idx:05d}.png")
         print(f"Image Saved as: {control.OUT_dir}/{control.IN_batch}_{img_idx:05d}.png")
-        # control.IN_idx+=1
+        img_idx+=1 
         # show the image
         # img.show()
         pil_image0 = IN_img
@@ -214,13 +214,15 @@ def generate_control(control):
         new_image.paste(pil_image2, (pil_image1.width, 0))
         new_image.paste(pil_image0, (pil_image1.width + pil_image2.width, 0))
         new_image_dir = os.path.join(control.OUT_dir, "merged_images")
+        
         if not os.path.exists(new_image_dir):
           os.makedirs(new_image_dir)
-        display.clear_output(wait=True)
+
         # # Show the resulting image
         new_image.show()
         new_image.save(os.path.join(new_image_dir, f"new_image_{img_idx:05d}.png"))
         display.clear_output(wait=True)
+        # display.clear_output(wait=True)
         if img_idx % control.render_video_every == 0:
             print("..\033[33mRendering Video\033[0m..")
             time_start = time.time()
@@ -232,6 +234,8 @@ def generate_control(control):
             print(f"Progress Animation Video Compiled, Saved to: {control.OUT_dir}, Filename: {output_filename}")
             print(f"Video Rendered in: {time_elapsed} seconds..")
     if img_idx == max_frames:
+        frame_folder = control.OUT_dir
         output_filename_final = f"{control.OUT_dir}/{control.IN_batch}_final.mp4"
         create_first_video(frame_folder, output_filename_final, frame_rate=30, quality=17)
         print(f"Animation Video Compled, Saved to: {control.OUT_dir}, Filename: {output_filename_final}")
+        
