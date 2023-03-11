025373642826e201eedd3676dd338c45bdcc0055
AI Manifest <108227908+AIManifest@users.noreply.github.com>
2023-03-11 14:28:11 +1000

    Update controlnet.py

--- helpers/controlnet.py
+++ helpers/controlnet.py
@@ -222,7 +222,7 @@ def generate_control(root, control):
         # # Show the resulting image
         new_image.show()
         new_image.save(os.path.join(new_image_dir, f"new_image_{img_idx:05d}.png"))
-        display.clear_output(wait=False)
+        display.clear_output(wait=True)
         # display.clear_output(wait=True)
         if img_idx % control.render_video_every == 0:
             print("..\033[33mRendering Video\033[0m..")
