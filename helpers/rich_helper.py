36ee1b7373f1ff955541bdca8e3426af67f28c31
AI Manifest <108227908+AIManifest@users.noreply.github.com>
2023-03-19 12:24:31 +1000

    Update rich_helper.py

--- helpers/rich_helper.py
+++ helpers/rich_helper.py
@@ -13,32 +13,57 @@ def print_animation_table(args, anim_args, keys, frame_idx):
     keys = DeformAnimKeys(anim_args)
     console = Console()
 
-    table = Table(show_header=True, header_style="bold green", box=box.ROUNDED)
-    table.add_column("Parameters", justify="left", style="green")
-    table.add_column("Value", justify="left", style="yellow")
-    
-    table.add_row("Steps", str(int(keys.steps_schedule_series[frame_idx])))
-    table.add_row("Scale", str(args.scale))
-    table.add_row("Sampler", str(args.sampler))
-    table.add_row("Angle", f"{keys.angle_series[frame_idx]}")
-    table.add_row("Zoom", f"{keys.zoom_series[frame_idx]}")
-    table.add_row("Tx", f"{keys.translation_x_series[frame_idx]}")
-    table.add_row("Ty", f"{keys.translation_y_series[frame_idx]}")
-    table.add_row("Tz", f"{keys.translation_z_series[frame_idx]}")
-    table.add_row("Rx", f"{keys.rotation_3d_x_series[frame_idx]}")
-    table.add_row("Ry", f"{keys.rotation_3d_y_series[frame_idx]}")
-    table.add_row("Rz", f"{keys.rotation_3d_z_series[frame_idx]}")
-    table.add_row("Rw", f"{keys.rotation_3d_w_series[frame_idx]}")
-    table.add_row("Aspect Ratio", f"{keys.aspect_ratio_series[frame_idx]}")
-    table.add_row("Near Plane", f"{keys.near_series[frame_idx]}")
-    table.add_row("Far Plane", f"{keys.far_series[frame_idx]}")
-    table.add_row("Field Of View", f"{keys.fov_series[frame_idx]}")
-    table.add_row("Noise", f"{keys.noise_schedule_series[frame_idx]}")
-    table.add_row("Strength", f"{keys.strength_schedule_series[frame_idx]}")
-    table.add_row("Contrast", f"{keys.contrast_schedule_series[frame_idx]}")
-    table.add_row("Kernel", str(int(keys.kernel_schedule_series[frame_idx])))
-    table.add_row("Sigma", f"{keys.sigma_schedule_series[frame_idx]}")
-    table.add_row("Amount", f"{keys.amount_schedule_series[frame_idx]}")
-    table.add_row("Threshold", f"{keys.threshold_schedule_series[frame_idx]}")
+    table1 = Table(show_header=True, header_style="bold green", box=box.ASCII_DOUBLE_HEAD)
+    table1.add_column("Steps", justify="left")
+    table1.add_column("Scale", justify="left")
+    table1.add_column("Sampler", justify="left")
+    table1.add_column("Angle", justify="left")
+    table1.add_column("Zoom", justify="left")
+    table1.add_column("Tx", justify="left")
+    table1.add_column("Ty", justify="left")
+    table1.add_column("Tz", justify="left")
+    table1.add_column("Rx", justify="left")
+    table1.add_column("Ry", justify="left")
+    table1.add_column("Rz", justify="left")
+    table1.add_column("Rw", justify="left")
+    table1.add_column("Aspect Ratio", justify="left")
 
-    console.print(table)
+    table2 = Table(show_header=True, header_style="bold green", box=box.ASCII_DOUBLE_HEAD)
+    table2.add_column("Near Plane", justify="left")
+    table2.add_column("Far Plane", justify="left")
+    table2.add_column("Field Of View", justify="left")
+    table2.add_column("Noise", justify="left")
+    table2.add_column("Strength", justify="left")
+    table2.add_column("Contrast", justify="left")
+    table2.add_column("Kernel", justify="left")
+    table2.add_column("Sigma", justify="left")
+    table2.add_column("Amount", justify="left")
+    table2.add_column("Threshold", justify="left")
+
+    table1.add_row(str(int(keys.steps_schedule_series[frame_idx])),
+                  str(args.scale),
+                  str(args.sampler),
+                  f"{keys.angle_series[frame_idx]}",
+                  f"{keys.zoom_series[frame_idx]}",
+                  f"{keys.translation_x_series[frame_idx]}",
+                  f"{keys.translation_y_series[frame_idx]}",
+                  f"{keys.translation_z_series[frame_idx]}",
+                  f"{keys.rotation_3d_x_series[frame_idx]}",
+                  f"{keys.rotation_3d_y_series[frame_idx]}",
+                  f"{keys.rotation_3d_z_series[frame_idx]}",
+                  f"{keys.rotation_3d_w_series[frame_idx]}",
+                  f"{keys.threshold_schedule_series[frame_idx]}")
+
+    table2.add_row(f"{keys.aspect_ratio_series[frame_idx]}",
+                  f"{keys.near_series[frame_idx]}",
+                  f"{keys.far_series[frame_idx]}",
+                  f"{keys.fov_series[frame_idx]}",
+                  f"{keys.noise_schedule_series[frame_idx]}",
+                  f"{keys.strength_schedule_series[frame_idx]}",
+                  f"{keys.contrast_schedule_series[frame_idx]}",
+                  str(int(keys.kernel_schedule_series[frame_idx])),
+                  f"{keys.sigma_schedule_series[frame_idx]}",
+                  f"{keys.amount_schedule_series[frame_idx]}")
+                  
+    console.print(table1)
+    console.print(table2)
