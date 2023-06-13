2e95309f83a885422ae5ec6907df137300646c02
AI Manifest <108227908+AIManifest@users.noreply.github.com>
2023-06-13 16:21:40 +1000

    Update audio_parser_helper.py

--- helpers/audio_parser_helper.py
+++ helpers/audio_parser_helper.py
@@ -10,6 +10,7 @@ import json
 import os
 import re
 import string
+import subprocess
 from subprocess import Popen, PIPE
 import textwrap
 import time
@@ -21,18 +22,7 @@ import pandas as pd
 import panel as pn
 from tqdm.auto import tqdm
 
-import tokenizations
-import webvtt
 import whisper
-
-from vktrs.utils import remove_punctuation
-from vktrs.utils import get_audio_duration_seconds
-from vktrs.youtube import (
-    YoutubeHelper,
-    parse_timestamp,
-    vtt_to_token_timestamps,
-    srv2_to_token_timestamps,
-)
 
 import io
 from pathlib import Path
@@ -82,7 +72,7 @@ def copy_process_streams(process: sp.Popen):
 def separate_audio_stems(music_video_args):
     inp = music_video_args.in_path
     outp = music_video_args.out_path
-    cmd = ["python3", "-m", "demucs.separate", "-o", str(outp), "-n", music_video_args.model]
+    cmd = ["python", "-m", "demucs.separate", "-o", str(outp), "-n", music_video_args.model]
     if music_video_args.mp3:
         cmd += ["--mp3", f"--mp3-bitrate={music_video_args.mp3_rate}"]
     if music_video_args.float32:
@@ -105,63 +95,55 @@ def separate_audio_stems(music_video_args):
         print("Command failed, something went wrong.")
     torch.cuda.empty_cache()
 
+def get_audio_duration_seconds(audio_fpath):
+        outv = subprocess.run([
+            'ffprobe'
+            ,'-i',audio_fpath
+            ,'-show_entries', 'format=duration'
+            ,'-v','quiet'
+            ,'-of','csv=p=0'
+            ],
+            stdout=subprocess.PIPE
+            ).stdout.decode('utf-8')
+        return float(outv.strip())
+
 def create_music_video_animation_args(root, music_video_args):
     if music_video_args.yt_video_url:
-        print("Initializing Audio Analysis")
+        print("\033[38;2;255;192;203m--Initializing Audio Analysis--")
         audio_root = os.path.join(root.output_path_gdrive, "audio")
+        audio_root = Path(audio_root)
         os.makedirs(audio_root, exist_ok=True)
+        storyboard = OmegaConf.create()
         # check if user provided an audio filepath (or we already have one from youtube) before attempting to download
         if music_video_args.audio_fpath is None:
-            helper = YoutubeHelper(
-                music_video_args.yt_video_url,
-                ydl_opts = {
-                    'outtmpl':{'default':os.path.join(audio_root, f"ytdlp_content.%(ext)s" )},
-                    'writeautomaticsub':True,
-                    'subtitlesformat':'srv2/vtt'
-                    },
+            storyboard = OmegaConf.create()
+
+            d_ = dict(
+                # all the underscore does is make it so each of the following lines can be preceded with a comma
+                # otw the first parameter would be offset from the other in the colab form
+                _=""
+
+                , video_url = music_video_args.yt_video_url # @param {type:'string'}
+                , audio_fpath = '' # @param {type:'string'}
             )
 
-            # estimate video end
-            video_duration = dt.timedelta(seconds=helper.info['duration'])
-            music_video_args.video_duration = video_duration.total_seconds()
+            # @markdown `video_url` - URL of a youtube video to download as a source for audio and potentially for text transcription as well.
 
-            audio_fpath = audio_root + "/audio.mp3"
-            input_audio = helper.info['requested_downloads'][-1]['filepath']
-            runffmpeg = Popen(['ffmpeg',
-                                    '-y', '-i', 
-                                    input_audio, 
-                                    '-acodec', 'libmp3lame', 
-                                    audio_fpath], 
-                                    stdout=PIPE, stderr=PIPE)
-            foutput, ferror = runffmpeg.communicate()
-            if runffmpeg.returncode != 0:
-                print(ferror)
-                raise RuntimeError(ferror)            
+            # @markdown `audio_fpath` - Optionally provide an audio file instead of relying on a youtube download. Name it something other than 'audio.mp3', 
+            # @markdown                 otherwise it might get overwritten accidentally.
 
-            # to do: write audio and subtitle paths/meta to storyboard
-            music_video_args.audio_fpath = audio_fpath
 
-            if False:
-                subtitle_format = helper.info['requested_subtitles']['en']['ext']
-                subtitle_fpath = helper.info['requested_subtitles']['en']['filepath']
+            d_.pop('_')
+            storyboard.params = d_
+            
+    # to do: write audio and subtitle paths/meta to storyboard
+    audio_fpath = str( audio_root / 'audio.m4a' )
+    music_video_args.audio_fpath = audio_fpath
 
-                if subtitle_format == 'srv2':
-                    with open(subtitle_fpath, 'r') as f:
-                        srv2_xml = f.read() 
-                    token_start_times = srv2_to_token_timestamps(srv2_xml)
-                    # to do: handle timedeltas...
-                    #storyboard.params.token_start_times = token_start_times
+    storyboard_fname = audio_root / 'storyboard.yaml'
+    with open(storyboard_fname,'wb') as fp:
+        OmegaConf.save(config=storyboard, f=fp.name)
 
-                elif subtitle_format == 'vtt':
-                    captions = webvtt.read(subtitle_fpath)
-                    token_start_times = vtt_to_token_timestamps(captions)
-                    # to do: handle timedeltas...
-                    #storyboard.params.token_start_times = token_start_times
-
-                # If unable to download supported subtitles, force use whisper
-                else:
-                    music_video_args.whisper_seg = True
-    
     if music_video_args.video_duration is None:
         # estimate duration from audio file
         audio_fpath = music_video_args.audio_fpath
@@ -178,101 +160,56 @@ def create_music_video_animation_args(root, music_video_args):
 
     whisper_seg = music_video_args.whisper_seg
 
+    storyboard = OmegaConf.create()
+
     if 'hf_helper' in locals():
         del hf_helper.img2img
         del hf_helper.text2img
         del hf_helper
-
+    
+    gc.collect()
+    torch.cuda.empty_cache()
 
     if whisper_seg:
-        from vktrs.asr import (
-            #whisper_lyrics,
-            #whisper_transcribe,
-            #whisper_align,
-            whisper_transmit_meta_across_alignment,
-            whisper_segment_transcription,
-        )
+        storyboard_fname = audio_root / 'storyboard.yaml'
+        running = subprocess.Popen(["whisper", 
+                "--model", 
+                "large", 
+                "--word_timestamps", 
+                "True", "-o", str(audio_root), audio_fpath], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output, error = running.communicate()
+        print(output, error)
 
-        #prompt_starts = whisper_lyrics(audio_fpath=storyboard.params.audio_fpath)
+        # outputs text files as audio.* locally
+        #with Path('audio.json').open() as f:
+        whisper_seg_fpath = Path(music_video_args.audio_fpath).with_suffix('.json')
+        whisper_seg_fpath = audio_root / whisper_seg_fpath
+        with whisper_seg_fpath.open() as f:
+            timings = json.load(f)
 
-        audio_fpath = music_video_args.audio_fpath
-        #whispers = whisper_transcribe(audio_fpath)
 
-        # to do: dropdown selectors
-        segmentation_model = 'tiny'
-        transcription_model = 'large'
+        # TODO: this doesn't need to be a function...
+        def whisper_segments_to_vktrs_promptstarts(segments):
+            for rec in segments:
+                rec['ts'] = rec['start']
+                rec['prompt'] = rec['text']
+            return segments
 
-        music_video_args.whisper = dict(
-            segmentation_model = segmentation_model
-            ,transcription_model = transcription_model
-        )
+        prompt_starts = whisper_segments_to_vktrs_promptstarts(timings['segments'])
 
-        whispers = {
-            #'tiny':None, # 5.83 s
-            #'large':None # 3.73 s
-        }
-        # accelerated runtime required for whisper
-        # to do: pypi package for whisper
+        # i don't think this is reliable unfortunately.
+        #storyboard.params['video_duration'] = storyboard.prompt_starts[-1]['end']
+        video_duration = get_audio_duration_seconds(audio_fpath)
 
-        # to do: use transcripts we've already built if we have them
-        #scripts = storyboard.params.whisper.get('transcriptions')
-        
-        for k in set([segmentation_model, transcription_model]):
-            #if k in scripts:
+        # prompt_starts = storyboard.prompt_starts
 
-            options = whisper.DecodingOptions(
-                language='en',
-            )
-            # to do: be more proactive about cleaning up these models when we're done with them
-            model = whisper.load_model(k).to('cuda')
-            start = time.time()
-            print(f"Transcribing audio with whisper-{k}")
-            
-            # to do: calling transcribe like this unnecessarily re-processes audio each time.
-            whispers[k] = model.transcribe(audio_fpath) # re-processes audio each time, ~10s overhead?
-            print(f"elapsed: {time.time()-start}")
-            del model
-            gc.collect()
+        ### checkpoint the processing work we've done to this point
 
-        transcriptions = {}
-        transcription_root = os.path.join(root.output_path_gdrive, 'whispers')
-        os.makedirs(transcription_root, exist_ok=True)
-        writer = whisper.utils.get_writer(output_format='vtt', output_dir=transcription_root) # output dir doesn't do anything...?
-        for k in whispers:
-            outpath = os.path.join(transcription_root, f"{k}.vtt")
-            transcriptions[k] = outpath
-            with open(outpath,'w') as f:
-                # to do: upstream PR to control verbosity
-                writer.write_result(
-                    whispers[k],
-                    file=f,
-                )
+        prompt_starts_copy = copy.deepcopy(prompt_starts)
+        prompt_starts = prompt_starts_copy
 
-        # music_video_args.whisper.transcriptions = transcriptions
-
-        #tiny2large, large2tiny, whispers_tokens = whisper_align(whispers)
-        # sanitize and tokenize
-        whispers_tokens = {}
-        for k in whispers:
-            whispers_tokens[k] = [
-            remove_punctuation(tok) for tok in whispers[k]['text'].split()
-            ]
-
-        # align sequences
-        tiny2large, large2tiny = tokenizations.get_alignments(
-            whispers_tokens[segmentation_model], #whispers_tokens['tiny'],
-            whispers_tokens[transcription_model] #whispers_tokens['large']
-        )
-        #return tiny2large, large2tiny, whispers_tokens
-
-        token_large_index_segmentations = whisper_transmit_meta_across_alignment(
-            whispers,
-            large2tiny,
-            whispers_tokens,
-        )
-        prompt_starts = whisper_segment_transcription(
-            token_large_index_segmentations,
-        )
+        with open(storyboard_fname) as fp:
+            OmegaConf.save(config=storyboard, f=fp.name)
 
         prompt_starts_copy = copy.deepcopy(prompt_starts)
         
@@ -338,7 +275,7 @@ def create_music_video_animation_args(root, music_video_args):
 def get_keyframes_total_for_animation(music_video_args):
     audio, sr = librosa.load("/content/drive/MyDrive/AI/StableDiffusion/audio/htdemucs/audio/drums.mp3")
 
-    rms = librosa.feature.rms(audio)[0]
+    rms = librosa.feature.rms(y=audio)[0]
 
     threshold = 0.1
 
